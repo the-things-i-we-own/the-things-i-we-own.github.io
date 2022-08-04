4ce477b7c9ec3b57cbd0b87b393a7f2ce97cd10f
pe-hu <we.are.pe.hu@gmail.com>
2022-08-04 17:22:40 +0900

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ fclose($fp);
 <html lang="ja">
 
 <head>
-    <title>The Things nk Own</title>
+    <title>The Things NK Own</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
