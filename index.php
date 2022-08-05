96046ecdb7c9a37895b81df5daef378123471820
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-05 15:20:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -381,14 +381,14 @@ fclose($fp);
         <div id="server">
             <p>
                 <?php
-                    echo $_SERVER['SERVER_NAME'];
+                    echo $_SERVER['HTTP_HOST'];
                     echo $_SERVER['REQUEST_URI'];
                 ?>
                 <br/>
                 <?php
-                    echo $_SERVER['SERVER_PROTOCOL'];
                     echo $_SERVER['SERVER_ADDR'];
-                    echo $_SERVER['HTTP_HOST'];
+                    echo $_SERVER['SERVER_PORT'];
+                    echo $_SERVER['SERVER_PROTOCOL'];
                     echo $_SERVER['HTTPS'];
                 ?>
             </p>
