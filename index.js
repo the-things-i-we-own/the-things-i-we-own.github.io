50f17bd54e93ca4ae5cebf0759a618dfd1392f07
pe-hu <we.are.pe.hu@gmail.com>
2022-08-17 19:43:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-var Index = [
-    "このページの説明文を記述する。<br/>",
-    "説明文は、ページ右上のタイトルをクリックすると、流れる文字が入れ替わり表示される。",
-    "トップページでVIEW ALLをクリックすると、カバービジュアルの上に、説明文の全文が表示される。<br/>",
-    "説明文は、検索エンジンに表示される挨拶文としても使用する。"
-]
-
-function more() {
-    $("#header marquee").html(Index[Math.floor(Math.random() * Index.length)]);
-}
-
-function greeting() {
-    $("#greeting #text").html(Index);
-}
-
 var volume;
 var synth;
 var notes;
