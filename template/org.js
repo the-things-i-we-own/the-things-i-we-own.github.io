f15535e76c80645504f27e035b1a1861a25d1a73
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-08-21 16:51:29 +0900

    org

---
+++ template/org.js
@@ -0,0 +1,26 @@
+'use strict'
+
+window.addEventListener('load', (event) => {
+    let targets = document.querySelectorAll("#things ul li")
+    let filter = document.querySelectorAll('#org input[type="radio"]')
+    if (filter) {
+        //****** for all select ******
+        for (let i of filter) {
+            i.addEventListener('change', () => {
+                let value = i.value
+                let name = i.getAttribute('name')
+                //*** for each target ***
+                for (let ii of targets) {
+                    //*** delete hidden class ***
+                    ii.classList.remove('hidden')
+                    //*** check target every select ***
+                    let item_data = ii.getAttribute('data-' + name)
+                    //*** set hidden class ***
+                    if (value && value !== 'all' && value !== item_data && !ii.classList.contains('hidden')) {
+                        ii.classList.add('hidden')
+                    }
+                }
+            })
+        }
+    }
+});
